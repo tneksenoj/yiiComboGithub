efa152c54f982a31ae81729f371c40dd9eaaa3ba
marnold22 <minimearnold@gmail.com>
2016-06-28 11:21:25 -0700

    Initialized yii2-admin

--- yiiCombo/backend/config/main.php
+++ yiiCombo/backend/config/main.php
@@ -11,7 +11,12 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+          'admin' => [
+              'class' => 'mdm\admin\Module',
+              'layout' => 'left-menu',
+              ],
+      ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
@@ -26,17 +31,33 @@ return [
                 ],
             ],
         ],
+        'authManager' => [
+                'class' => 'yii\rbac\DbManager',
+                'defaultRoles' => ['guest'],
+        ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            //'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
+    /*'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'projects/*',
+            //'admin/*',
+            //'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+    ],*/
     'params' => $params,
 ];
