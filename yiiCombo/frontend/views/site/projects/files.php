307250921e2515397dc11936d0b6ce68e63a93d3
marnold22 <minimearnold@gmail.com>
2016-07-07 12:27:57 -0700

    Got rid of iFrame - using data object
    This is for displaying own cloud site within Yii site

--- yiiCombo/frontend/views/site/projects/files.php
+++ yiiCombo/frontend/views/site/projects/files.php
@@ -5,6 +5,7 @@ use yii\widgets\LinkPager;
 $this->title = 'Files';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<?php header('X-Frame-Options: GOFORIT'); ?>
 
 <!--  This page dynamically generates project tiles with a brief description  -->
 
@@ -12,8 +13,9 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <!--<a class="btn btn-md btn-primary pull-right proj-btn"> View My Projects </a>
     <a class="btn btn-md btn-primary pull-right proj-btn"> Add Project </a>-->
+<!--     <iframe src="https://ec2-54-218-30-182.us-west-2.compute.amazonaws.com/owncloud" width="1140" height="700">
+      </iframe> -->
 
-    <iframe src="https://ec2-54-218-30-182.us-west-2.compute.amazonaws.com/owncloud" width="1140" height="700">
-      </iframe>
+      <object data=https://localhost/owncloud width="1140" height="700"> <embed src=https://localhost/owncloud width="1140" height="700"> </embed></object>
 
 </div>
