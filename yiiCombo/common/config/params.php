e296887bddf9b4640f9d0611e58a10ba193cf4fd
marnold22 <minimearnold@gmail.com>
2016-07-07 15:50:03 -0700

    test

--- yiiCombo/common/config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'user.passwordResetTokenExpire' => 3600,
-    'OC_files' => 'owncloud/remote.php/files/Projects/',
-    'OCS' => '+++++++/owcloud/v1.php/cloud/',
-];
