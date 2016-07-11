ed2dd06df464d33f17d482a5cb0e15b3455af327
marnold22 <minimearnold@gmail.com>
2016-07-11 11:48:30 -0700

    Spacing

--- yiiCombo/backend/controllers/ProjectsController.php
+++ yiiCombo/backend/controllers/ProjectsController.php
@@ -121,7 +121,7 @@ class ProjectsController extends Controller
 
 
 
-    public function createProjectGroupOnOwncloud($groupName) 
+    public function createProjectGroupOnOwncloud($groupName)
     {
       $client = new WebClient();
       $response = $client->createRequest()
@@ -135,7 +135,7 @@ class ProjectsController extends Controller
 
 
 
-    public function deleteProjectGroupOnOwncloud($groupName) 
+    public function deleteProjectGroupOnOwncloud($groupName)
     {
       $client = new WebClient();
       $response = $client->createRequest()
@@ -160,12 +160,12 @@ class ProjectsController extends Controller
           if (Yii::$app->webdavFs->has(Yii::$app->params['OC_files'] . $model->Name)){
             throw new UserException('Sorry that name is already in use on the project server.');
           }
-          
+
           $model->file = UploadedFile::getInstance($model, 'file');
           $model->logo = 'uploads/' . $model->file->baseName . '.' . $model->file->extension;
 
           if ( $model->validate() ) {
-      
+
             if (!$model->save()) {
               throw new UserException('Sorry an error occured in your action create of the project controller. Please contact the administrator.');
             }
