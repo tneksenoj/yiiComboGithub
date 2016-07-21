39452895dab2cb1885266c969c5267d016f068b6
Kent Jones <tneksenoj@gmail.com>
2016-07-21 12:11:08 -0700

    Moved assets to backend to prepare for backend project page changes.

---
+++ yiiCombo/backend/assets/SiiAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class SiiAsset extends AssetBundle
+{
+    public $sourcePath = '@app/assets';
+    public $basePath = null;
+    public $baseUrl = null;
+
+    public $css = [
+        'yii2-sii-file-mgr.css'
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'backend\assets\W3schoolsAsset',
+    ];
+}
---
+++ yiiCombo/backend/assets/W3schoolsAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class W3schoolsAsset extends AssetBundle
+{
+    public $sourcePath = '@app/assets';
+    public $basePath = null;
+    public $baseUrl = null;
+
+    public $css = [
+        'w3.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'backend\assets\AppAsset',
+    ];
+}
