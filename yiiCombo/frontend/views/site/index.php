bb233dbee762ca54c5e6f2c95b5c5529e64e7ee2
root <root@lab-sci-304-14.whitworth.edu>
2016-07-25 13:50:23 -0700

    fixed the index page links.

--- yiiCombo/frontend/views/site/index.php
+++ yiiCombo/frontend/views/site/index.php
@@ -15,18 +15,18 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="site-index" id="sii-sciencebg">
   <div class="w3-row">
-    <div class="w3-full spacer"> 
+    <div class="w3-full spacer">
     </div>
     <div class="w3-row">
-      <div class="w3-full w3-center"> 
+      <div class="w3-full w3-center">
         <div class="w3-container">
           <img id="sii-splash-image" src="<?php echo Yii::getAlias('@web') . '/images/white.png'; ?>" width="622" height="131">
         </div>
-      </div> 
+      </div>
       <div class="w3-full w3-center">
           <h4 id="sii-dynam">This is a dynamic inter-collegiate biology research sharing platform.</h4>
-          <p><a class="btn btn-primary btn-lg" href=<?php echo Yii::getAlias('@web') . "/index.php?r=site%2Fabout"; ?>>Learn More</a>
-          <a class="btn btn-primary btn-lg" href=<?php echo Yii::getAlias('@web') . "/index.php/signup?r=site%2Fsignup"; ?>>Request Access</a></p>
+          <p><a class="btn btn-primary btn-lg" href=<?php echo Yii::getAlias('@web') . "/index.php/site/about"; ?>>Learn More</a>
+          <a class="btn btn-primary btn-lg" href=<?php echo Yii::getAlias('@web') . "/index.php/site/signup"; ?>>Request Access</a></p>
       </div>
     </div>
   </div>
