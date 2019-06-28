382faa4a2a41d5e8ca5687c3b7be55719cae22aa
Alyssaf21 <a.c.fusato@gmail.com>
2019-06-27 17:05:20 -0700

    Fixed create owncloud user for project sharing

--- yiiCombo/backend/models/Requests.php
+++ yiiCombo/backend/models/Requests.php
@@ -69,34 +69,86 @@ class Requests extends \yii\db\ActiveRecord
     //   return ( $data != null );
     // }
 
-    public static function createOwncloudUser($username)
+    public static function createOwncloudUser($username) //Owncloud database
     {
       // Get Information about user from Yiicombo
       //$user = User::find()->where(['username' => $username])->one();
-      $user = User::findOne(['username' => $username]);
+      // This gets a user from the Yiicombo database.  This username and password will be used
+      // to set the username and password on the owncloud side of things.
+      $user = User::findOne(['username' => $username]); // Finding user based on username
 
-      if( ! OcUsers::find()->where(['uid' => $user->username])->exists() )
-      {
+      // Now, search the owncloud database for the same user, if they don't exist, create that user
+      // and give them the same username and password as is in the Yiicombo database.
+      if( ! OcUsers::find()->where(['uid' => $user->username])->exists() ) //Create user with CURL on owncloud user provisioning API THEN check if share permissions works correctly 
+      {//If user doesn't exist, create a new user
+        // NEW: Use the Owncloud User API and webclient interface to create a user with the 
+        // username passed into this function.  Give the user a temporary random passworkd
+        // that will be reset shortly. 
+        $temppass = Yii::$app->security->generateRandomString();
+    
+        $client = new WebClient( //$client is a newly created web client formatted in JSON
+          [
+              'responseConfig' => [
+                  'format' => WebClient::FORMAT_JSON
+              ],
+          ]
+        );
+        $response = $client->createRequest() //Method used to create new HTTP request
+        ->setMethod('POST')
+        ->setUrl(yiicfg::OCS . 'users')
+        ->setData(['userid' => $username, 
+                    'password' => $temppass, // Default to least permission 
+                  ])
+        ->setOptions(['timeout' => 5,])
+        ->send();
+        //Yii::error("\n" . "OCS_SHARE was: ". yiicfg::OCS_SHARE . 'shares');
+        $p = xml_parser_create();
+        xml_parse_into_struct($p, $response->content, $vals, $index);
+        xml_parser_free($p);
+        Yii::error("\n" . "Vals is: ". json_encode($vals));
+        if (! $vals[$index['STATUS'][0]]['value'] == "ok") {
+          throw new UserException('Sorry couldn\'t create user on OwnCloud.');
+        } 
+        // Now continue with original method of adding user to Owncloud
+        // This should overwrite the temporary password
         // Add Username and Password to OC
-        $ocuser = new OcUsers();
-        $ocuser->uid = $user->username;
-        $ocuser->password = '1|'. $user->password_hash;
-        if(!$ocuser->save()) {
-            throw new UserException('Sorry there was an error creating user on OwnCloud.');
-        }
-        // Add email-address to OC for password recovery
-        $ocpref = new OcPreferences();
-        $ocpref->userid = $user->username;
-        $ocpref->appid = 'settigs';
-        $ocpref->configkey = 'email';
-        $ocpref->configvalue = $user->email;
-        if(!$ocpref->save()) {
-            throw new UserException('Sorry there was an error creating user on OwnCloud.');
-        }
+        User::changeUserPasswordOnOwncloud($username);
       }
     }
 
+    public function shareOCFolderWithUser($username, $projectname) {
+      
+      $client = new WebClient( //$client is a newly created web client formatted in JSON
+        [
+            'responseConfig' => [
+                'format' => WebClient::FORMAT_JSON
+            ],
+        ]
+      );
+      //curl -X POST /shares -d path="/Projects/Land" -d shareType=1 -d shareWith="Land"
 
+      $response = $client->createRequest() //Method used to create new HTTP request
+        ->setMethod('POST')
+        ->setUrl(yiicfg::OCS_SHARE . 'shares')
+        ->setData(['path' => '/Projects/' . $projectname, 'shareType' => 0, 'shareWith' => $username,
+                    'permissions' => 1, // Default to least permission 
+                  ])
+        ->setOptions(['timeout' => 5,])
+        ->send();
+        Yii::error("\n" . "OCS_SHARE was: ". yiicfg::OCS_SHARE . 'shares');
+        $p = xml_parser_create();
+        xml_parse_into_struct($p, $response->content, $vals, $index);
+        xml_parser_free($p);
+        Yii::error("\n" . "Vals is: ". json_encode($vals));
+        if ( $vals[$index['STATUS'][0]]['value'] == "ok") {
+          return true;
+        } else {
+          return false;
+        }
+        
+    }
+
+/*
     public function shareOCFolderWithUser($username, $projectname) {
       $client = new WebClient(
         [
@@ -111,21 +163,23 @@ class Requests extends \yii\db\ActiveRecord
         ->setMethod('post')
         ->setUrl(yiicfg::OCS_SHARE . 'shares')
         ->setData(['path' => '/Projects/' . $projectname, 'shareType' => 0, 'shareWith' => $username,
-                    'permissions' => 1, /* Default to least permission */
+                    'permissions' => 1, // Default to least permission 
                   ])
         ->setOptions(['timeout' => 5,])
         ->send();
-
+        Yii::error("\n" . "OCS_SHARE was: ". yiicfg::OCS_SHARE . 'shares');
         $p = xml_parser_create();
         xml_parse_into_struct($p, $response->content, $vals, $index);
         xml_parser_free($p);
-
+        Yii::error("\n" . "Vals is: ". json_encode($vals));
         if ( $vals[$index['STATUS'][0]]['value'] == "ok") {
           return true;
         } else {
           return false;
         }
-    }
+}*/
+
+  
 
 /*
     public static function deleteUserFromGroup($username, $groupName)
